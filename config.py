9b24ab7076bade760fc51d88fcc2445d6e0e76d0
Yamile <solisyam927@gmail.com>
2026-02-14 21:21:09 -0600

    Lista de alumnos 14/02/2026

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+
+class Config(object):
+    SECRET_KEY ="ClaveSecreta"
+    SESSION_COOKIE_SECURE=False
+    
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost:3306/bdidgs804'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    
